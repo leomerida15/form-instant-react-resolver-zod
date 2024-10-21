aadbeebce5bb855c67ffd078dd4e3b4be725c016
leomerida15 <dimasmerida15@gmail.com>
2024-10-21 01:57:31 -0400

    v0.0.3-alpha

--- src/context.tsx
+++ src/context.tsx
@@ -1,4 +1,6 @@
-import { createContext } from "use-context-selector";
-import { zodResolverProps } from "./type";
+import { createContext } from 'use-context-selector';
+import { zodResolverProps } from './type';
 
-export const ZodResolverContext = createContext<zodResolverProps>({} as zodResolverProps);
+export const ZodResolverContext = createContext<zodResolverProps>(
+  {} as zodResolverProps
+);
