3637823aca1e2cd362fbfe255ca8fede2d613427
leomerida15 <dimasmerida15@gmail.com>
2024-10-22 16:30:20 -0400

    0.0.10-alpha

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,5 @@
-import { FieldConfig } from "@form-instant/react-input-mapping";
-import { SuperRefineFunction } from "./funcs/types";
-
 export { Element as FormInstantElement } from "./element";
+export { createZodSchemaFieldConfig } from "./funcs/field-config";
 export { Provider as FormInstantProvider } from "./provider";
 export * from "./type";
 export { useFields, useSchema } from "./useSchema";
-export const createZodSchemaFieldConfig =
-    <AdditionalRenderable = null, FieldTypes = string>() =>
-    (config: FieldConfig<AdditionalRenderable, FieldTypes>): SuperRefineFunction => {
-        const refinementFunction: SuperRefineFunction = () => {
-            // Do nothing.
-        };
-
-        // @ts-expect-error This is a symbol and not a real value.
-        refinementFunction[FIELD_CONFIG_SYMBOL] = config;
-
-        return refinementFunction;
-    };
