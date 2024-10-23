ed6de7b98b1a7a1169ab0c2b014302e4e491f5de
leomerida15 <dimasmerida15@gmail.com>
2024-10-23 09:15:57 -0400

    0.0.14-alpha

--- src/element.tsx
+++ src/element.tsx
@@ -1,5 +1,5 @@
 import { useInputMapping } from "@form-instant/react-input-mapping";
-import { useId } from "react";
+import { createElement, useId } from "react";
 import { useFields } from "./useSchema";
 
 export interface ElementProps<Schema extends Record<string, any>> {
@@ -17,11 +17,13 @@ export const FormInstantElement = <S extends Record<string, any>>({ name }: Elem
     return (
         <>
             {inputs.map((props) => {
-                const Element = InputMapping.get(props.type)!;
+                const Element = InputMapping.get(props.type);
 
                 const { key, ...prop } = props;
 
-                return <Element {...prop} key={`${key}-${id}`} />;
+                if (!Element) return null;
+
+                return createElement(Element, { ...prop, key: `${key}-${id}` });
             })}
         </>
     );
