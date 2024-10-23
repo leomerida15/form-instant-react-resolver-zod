cc60173cf6cb4c7060a35abafd37bf586a4c6c13
leomerida15 <dimasmerida15@gmail.com>
2024-10-23 19:05:25 -0400

    add element component pre object inpt

--- src/element.tsx
+++ src/element.tsx
@@ -1,5 +1,5 @@
-import { useInputMapping } from '@form-instant/react-input-mapping';
-import { createElement, useId } from 'react';
+import { ElementMapping } from '@form-instant/react-input-mapping';
+import { useId } from 'react';
 import { useFields } from './useSchema';
 
 export interface ElementProps<Schema extends Record<string, any>> {
@@ -7,7 +7,6 @@ export interface ElementProps<Schema extends Record<string, any>> {
 }
 
 export const FormInstantElement = <S extends Record<string, any>>({ name }: ElementProps<S>) => {
-    const InputMapping = useInputMapping();
     const field = useFields(name);
 
     const inputs = Object.values(field.schema!);
@@ -17,13 +16,7 @@ export const FormInstantElement = <S extends Record<string, any>>({ name }: Elem
     return (
         <>
             {inputs.map((props) => {
-                const Element = InputMapping.get(props.type);
-
-                const { key, ...prop } = props;
-
-                if (!Element) return null;
-
-                return createElement(Element, { ...prop, key: `${key}-${id}` });
+                return <ElementMapping {...props} key={`${id}-${props.key}`} />;
             })}
         </>
     );
