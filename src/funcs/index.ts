38fecb2f619336650d447e8a9a0435ba213284d6
leomerida15 <dimasmerida15@gmail.com>
2024-11-23 14:36:21 -0400

    add extendZodWithFieldConfig

---
+++ src/funcs/index.ts
@@ -0,0 +1,5 @@
+export * from './default-values';
+export * from './field-config';
+export * from './field-type-inference';
+export * from './schema-parser';
+export * from './types';
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export * from './context';
 export * from './element';
+export * from './funcs';
+export * from './global.d';
 export * from './provider';
 export * from './type';
 export * from './useSchema';
