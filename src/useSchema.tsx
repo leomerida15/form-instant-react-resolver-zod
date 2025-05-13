df07e5049245de1f17b698d998b931826456a9d8
leomerida15 <dimasmerida15@gmail.com>
2025-05-13 11:15:15 -0400

    add initialValues in useSchema

--- src/useSchema.tsx
+++ src/useSchema.tsx
@@ -1,8 +1,8 @@
+import { useMemo } from 'react';
 import { useContext } from 'use-context-selector';
 import { z, ZodEffects, ZodObject } from 'zod';
 import { ZodResolverContext } from './context';
 import { parseSchema } from './funcs/schema-parser';
-import { useMemo } from 'react';
 
 const useSchemaBase = () =>
     useContext(ZodResolverContext) as ZodObject<never, never> | ZodEffects<never, never>;
@@ -27,26 +27,26 @@ export const generateInitialValues = <S extends Record<string, any>>(schema: Dat
         const shape = (() => {
             if (schema instanceof z.ZodEffects) return schema.innerType().shape;
             if (schema instanceof z.ZodObject) return schema.shape;
-    
+
             const option = schema._def.optionsMap.get(dp[schema._def.discriminator]);
-    
+
             if (!option) throw new Error('No option found');
-    
+
             return option.shape;
         })();
-        
+
         const initialValues: Record<string, any> = {};
-    
+
         for (const key of Object.keys(shape)) {
             const fieldSchema = shape[key];
             const result = fieldSchema.safeParse(undefined);
-    
+
             // Si tiene valor por defecto
             if (result.success) {
                 initialValues[key] = result.data;
                 continue;
             }
-    
+
             // Inferir tipo para campos requeridos sin default
             if (fieldSchema instanceof z.ZodString) {
                 initialValues[key] = '';
@@ -62,7 +62,7 @@ export const generateInitialValues = <S extends Record<string, any>>(schema: Dat
                 initialValues[key] = generateInitialValues(fieldSchema, dp);
             } else if (fieldSchema instanceof z.ZodDiscriminatedUnion) {
                 const option = fieldSchema._def.optionsMap.get(dp[fieldSchema._def.discriminator]);
-    
+
                 if (option) {
                     initialValues[key] = generateInitialValues(option, dp);
                 } else {
@@ -72,15 +72,15 @@ export const generateInitialValues = <S extends Record<string, any>>(schema: Dat
                 initialValues[key] = null;
             }
         }
-    
+
         return initialValues as S;
-    } catch (error) {
+    } catch {
         return {} as S;
     }
 };
 
 export const useSchema = (cbP: (dp: DP, preData?: Data) => Data, dp: DP) => {
-    const schema = useMemo(() => cbP(dp).fieldConfig({ dp, ...cbP(dp).fieldConfig }), [dp]);
+    const schema = useMemo(() => cbP(dp).fieldConfig({ dp, ...cbP(dp).fieldConfig }), [cbP, dp]);
 
     const initialValues = useMemo(() => generateInitialValues(schema, dp), [schema, dp]);
 
