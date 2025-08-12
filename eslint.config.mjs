77dd97e7a8c2cca6723aeeb973d23b623e384135
leomerida15 <dimasmerida15@gmail.com>
2025-08-12 14:18:29 -0400

    v2.0.0

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,7 +16,7 @@ export default tseslint.config(
         },
         rules: {
             '@typescript-eslint/no-unused-vars': 'error',
-            '@typescript-eslint/no-explicit-any': 'warn',
+            '@typescript-eslint/no-explicit-any': 'off',
             '@typescript-eslint/explicit-function-return-type': 'off',
             '@typescript-eslint/explicit-module-boundary-types': 'off',
             '@typescript-eslint/no-non-null-assertion': 'warn',
